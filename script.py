7c67580a595a1bdafe5b2522ccb14619328069c7
Vladimir Gorbovskiy <80061175+jabooticaba@users.noreply.github.com>
2021-07-09 14:49:42 +0000

    repl running options, simple test added

--- script.py
+++ script.py
@@ -1,27 +1,39 @@
 # pytest -v --driver Chrome --driver-path /path/to//chromedriver.exe
-
-# pytest -v --driver Chrome --driver-path /webdriver/chrome/chromedriver.exe
-
+# pytest -v --driver Chrome --driver-path Selenium_testing/webdriver/chrome/chromedriver.exe
 # pytest -v --driver Chrome --driver-path C:\chromesetup\chromedriver.exe
-
-# #Simple assignment
-# from selenium.webdriver import Chrome
-
-# driver = Chrome()
-
-# #Or use the context manager
-# from selenium.webdriver import Chrome
-
-# with Chrome() as driver:
-#     #your code inside this indent
-
 # Chrome(executable_path='/webdriver/chrome/chromedriver')
 
-# import selenium
-# from selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
 
-# driver = webdriver.Chrome() #получение объекта веб-драйвера для нужного браузера
+# options for repl vm running:
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 
-# selenium.get('https://google.com')
 
-# selenium.quit()
+def test_search_example():
+  driver = webdriver.Chrome(options=chrome_options)
+  driver.get("https://google.com")
+
+  time.sleep(5)  # just for demo purposes, do NOT repeat it on real projects!
+
+    # Find the field for search text input:
+  search_input = driver.find_element_by_name('q')
+
+
+    # Enter the text for search:
+  search_input.clear()
+  search_input.send_keys('first test')
+
+  time.sleep(5)  # just for demo purposes, do NOT repeat it on real projects!
+
+    # Click Search:
+  search_button = driver.find_element_by_name('btnK')
+  search_button.submit()
+
+  time.sleep(5)  # just for demo purposes, do NOT repeat it on real projects!
+
+    # Make the screenshot of browser window:
+  driver.save_screenshot('result.png')
