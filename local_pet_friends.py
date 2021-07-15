60c1972e02fcf8f9eb2ffe17e600f5a2a866c15d
jabooticaba <jabuticaba600@gmail.com>
2021-07-15 23:43:21 +0700

    simple login test added

--- local_pet_friends.py
+++ local_pet_friends.py
@@ -1,4 +1,6 @@
 import time
+import pytest
+from selenium import webdriver
 
 def test_petfriends(web_browser):
     web_browser.get("https://petfriends1.herokuapp.com/")
@@ -27,3 +29,25 @@ def test_petfriends(web_browser):
 
     # time.sleep(4)  # just for demo purposes, do NOT repeat it on real projects!
     assert web_browser.current_url == 'https://petfriends1.herokuapp.com/all_pets',"login error"
+
+
+@pytest.fixture(autouse=True)
+def testing():
+   pytest.driver = webdriver.Chrome('./webdriver/chrome/chromedriver.exe')
+   # Переходим на страницу авторизации
+   pytest.driver.get('http://petfriends1.herokuapp.com/login')
+
+   yield
+
+   pytest.driver.quit()
+
+
+def test_show_my_pets():
+   # Вводим email
+   pytest.driver.find_element_by_id('email').send_keys('vasya@mail.com')
+   # Вводим пароль
+   pytest.driver.find_element_by_id('pass').send_keys('12345')
+   # Нажимаем на кнопку входа в аккаунт
+   pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
+   # Проверяем, что мы оказались на главной странице пользователя
+   assert pytest.driver.find_element_by_tag_name('h1').text == "PetFriends"
