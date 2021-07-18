642ea30c48ca900edf149ac8d38896744e6ba019
jabooticaba <jabuticaba600@gmail.com>
2021-07-19 00:50:26 +0700

    Add unique pets assertion

--- local_pet_friends.py
+++ local_pet_friends.py
@@ -3,6 +3,7 @@
 import time
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def test_petfriends(web_browser):
     web_browser.get("https://petfriends1.herokuapp.com/")
@@ -35,8 +36,11 @@ def test_petfriends(web_browser):
 
 @pytest.fixture(autouse=True)
 def testing():
-    pytest.driver = webdriver.Chrome('./webdriver/chrome/chromedriver.exe')
-    pytest.driver.maximize_window()
+    chrome_options = Options()
+    chrome_options.headless = True
+    chrome_options.add_argument("--window-size=1280,800")
+    pytest.driver = webdriver.Chrome('./webdriver/chrome/chromedriver.exe', options=chrome_options)
+
     # Переходим на страницу авторизации
     pytest.driver.get('http://petfriends1.herokuapp.com/login')
 
@@ -44,7 +48,6 @@ def testing():
 
     pytest.driver.quit()
 
-@pytest.mark.current
 def test_show_my_pets():
     # add email
     pytest.driver.find_element_by_id('email').send_keys('test123@yandex.ru')
@@ -62,21 +65,40 @@ def test_show_my_pets():
     assert title.get_attribute("innerText") == "PetFriends: My Pets"
 
     # сбор значений проверяемых элементов
-    images = pytest.driver.find_elements_by_css_selector("tbody tr th")
+    pet_number = len(pytest.driver.find_elements_by_css_selector("table.table-hover tbody > tr"))
+    images = pytest.driver.find_elements_by_css_selector("tbody tr th img")
     names = pytest.driver.find_elements_by_xpath("//div[@id='all_my_pets']/table/tbody/tr/td[1]")
     breeds = pytest.driver.find_elements_by_xpath("//div[@id='all_my_pets']/table/tbody/tr/td[2]")
     ages = pytest.driver.find_elements_by_xpath("//div[@id='all_my_pets']/table/tbody/tr/td[3]")
 
-    for i in range(len(names)):
-        assert images[i].get_attribute('src') != ''
+    images_counter = 0
+    names_set = set()
+    pets_list = []
+    for i in range(pet_number):
+        pet = []
+        pet.append(names[i].text), pet.append(breeds[i].text), pet.append(ages[i].text)
+
+        names_set.add(names[i].text)
+        if images[i].get_attribute('src') != '':
+            images_counter +=1
+        # assert images[i].get_attribute('src') != '' #TODO Need to add soft assert
         assert names[i].text != ''
         assert breeds[i].text != ''
         assert ages[i].text != ''
 
-    #assertion of pet number in statistics frame and number of pet cards
-    pet_number = len(pytest.driver.find_elements_by_css_selector("table.table-hover tbody > tr"))
+        if pet not in pets_list:
+            pets_list.append(pet)
+
+    # assertion of pet number in statistics frame and number of pet cards
     pet_number_stat = pytest.driver.find_elements_by_xpath("/html/body/div[1]//div[@class='.col-sm-4 left']")
-    # for i in (pet_number_stat):
-    #     print(i.text)
-    assert (str(pet_number_stat[0].text.split("\n")[1].split(" ")[1])) == pet_number
+    assert (int(pet_number_stat[0].text.split("\n")[1].split(" ")[1])) == pet_number
+
+    # More then a half of pets has photo number
+    assert images_counter >= pet_number / 2
+
+    # all pets has different names
+    assert len(names) == len(names_set)
+
+    # all pets are different
+    assert pet_number == len(pets_list)
 
