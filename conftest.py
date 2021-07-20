63a0455f2d23c999f2584b9f24e6687aa91c94eb
jabooticaba <jabuticaba600@gmail.com>
2021-07-20 18:03:54 +0700

    Test function and fixture reworked

--- conftest.py
+++ conftest.py
@@ -1,17 +1,17 @@
 import pytest
 import uuid
 
-@pytest.fixture
-def chrome_options(chrome_options):
-    chrome_options.binary_location = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
-    chrome_options.add_argument('--kiosk')
-    chrome_options.headless = True
-    return chrome_options
-
-
 # @pytest.fixture
-# def driver_args():
-#     return ['--log-level=DEBUG']
+# def chrome_options(chrome_options):
+#     chrome_options.binary_location = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+#     chrome_options.add_argument('--kiosk')
+#     chrome_options.headless = True
+#     return chrome_options
+
+
+@pytest.fixture
+def driver_args():
+    return ['--log-level=DEBUG']
 
 # @pytest.hookimpl(hookwrapper=True, tryfirst=True)
 # def pytest_runtest_makereport(item, call):
@@ -23,40 +23,40 @@ def chrome_options(chrome_options):
 #     setattr(item, "rep_" + rep.when, rep)
 #     return rep
 
-@pytest.fixture
-def web_browser(request, selenium):
-
-    browser = selenium
-    browser.set_window_size(1400, 1000)
-
-    # Return browser instance to test case:
-    yield browser
-
-    # Do teardown (this code will be executed after each test):
-
-    if request.node.rep_call.failed:
-        # Make the screen-shot if test failed:
-        try:
-            file_name = str(uuid.uuid4())
-            browser.execute_script("document.body.bgColor = 'white';")
-
-            # Make screen-shot for local debug:
-            browser.save_screenshot('screenshots/' + file_name + '.png')
-
-            # For happy debugging:
-            log_to_file = []
-
-            print('URL: ', browser.current_url)
-            print('Browser logs:')
-            for log in browser.get_log('browser'):
-                print(log)
-                log_to_file.append(log)
-
-            # Save browser logs and page source code to file
-            with open('logs/' + file_name + '.txt', 'w') as txt_file:
-                txt_file.write('Browser logs:' + str(log_to_file))
-                txt_file.write("\n" + "----------------------------- Page sourse -------------------------------" + "\n" + "\n")
-                txt_file.write(browser.page_source)
-
-        except:
-            pass  # just ignore any errors here
+# @pytest.fixture
+# def web_browser(request, selenium):
+#
+#     browser = selenium
+#     browser.set_window_size(1400, 1000)
+#
+#     # Return browser instance to test case:
+#     yield browser
+#
+#     # Do teardown (this code will be executed after each test):
+#
+#     if request.node.rep_call.failed:
+#         # Make the screen-shot if test failed:
+#         try:
+#             file_name = str(uuid.uuid4())
+#             browser.execute_script("document.body.bgColor = 'white';")
+#
+#             # Make screen-shot for local debug:
+#             browser.save_screenshot('screenshots/' + file_name + '.png')
+#
+#             # For happy debugging:
+#             log_to_file = []
+#
+#             print('URL: ', browser.current_url)
+#             print('Browser logs:')
+#             for log in browser.get_log('browser'):
+#                 print(log)
+#                 log_to_file.append(log)
+#
+#             # Save browser logs and page source code to file
+#             with open('logs/' + file_name + '.txt', 'w') as txt_file:
+#                 txt_file.write('Browser logs:' + str(log_to_file))
+#                 txt_file.write("\n" + "----------------------------- Page sourse -------------------------------" + "\n" + "\n")
+#                 txt_file.write(browser.page_source)
+#
+#         except:
+#             pass  # just ignore any errors here
--- local_pet_friends.py
+++ local_pet_friends.py
@@ -7,6 +7,78 @@ from delayed_assert import expect, assert_expectations
 import time
 
 
+@pytest.fixture(autouse=True)
+def testing():
+    chrome_options = Options()
+    chrome_options.headless = True
+    chrome_options.add_argument("--window-size=1280,800")
+    driver = webdriver.Chrome('./webdriver/chrome/chromedriver.exe', options=chrome_options)
+    driver.implicitly_wait(10)
+    # go to auth page
+    driver.get('http://petfriends1.herokuapp.com/login')
+
+    yield driver
+
+    driver.quit()
+
+
+def test_show_my_pets(testing):
+    # add email
+    testing.find_element_by_id('email').send_keys('test123@yandex.ru')
+    # add password
+    testing.find_element_by_id('pass').send_keys('123456')
+    # click submit button
+    testing.find_element_by_css_selector('button[type="submit"]').click()
+    # user main page check
+    assert testing.find_element_by_tag_name('h1').text == "PetFriends"
+    # go to /my_pets
+    testing.find_element_by_xpath('//li/a[@href="/my_pets"]').click()
+    # /my_pets-title check
+    assert testing.title == "PetFriends: My Pets"
+    title = testing.find_element_by_css_selector('head title')
+    assert title.get_attribute("innerText") == "PetFriends: My Pets"
+
+    # сбор значений проверяемых элементов
+    pet_number = len(testing.find_elements_by_css_selector("table.table-hover tbody > tr"))
+    images = testing.find_elements_by_css_selector("tbody tr th img")
+    names = testing.find_elements_by_xpath("//div[@id='all_my_pets']/table/tbody/tr/td[1]")
+    breeds = testing.find_elements_by_xpath("//div[@id='all_my_pets']/table/tbody/tr/td[2]")
+    ages = testing.find_elements_by_xpath("//div[@id='all_my_pets']/table/tbody/tr/td[3]")
+
+    images_counter = 0
+    names_set = set()
+    pets_list = set()
+    for i in range(pet_number):
+        
+        names_set.add(names[i].text)
+        pet = (names[i].text, breeds[i].text, ages[i].text)
+        pets_list.add(pet)
+        
+        if images[i].get_attribute('src') != '':
+            images_counter +=1
+        expect(images[i].get_attribute('src') != '', 'Pet has no picture')
+        expect(names[i].text != '', 'Empty name field')
+        expect(breeds[i].text != '', 'Empty name field')
+        expect(ages[i].text != '', 'Empty age field')
+
+    # all pets are displayed on the page
+    pet_number_stat = testing.find_elements_by_xpath("/html/body/div[1]//div[@class='.col-sm-4 left']")
+    expect((int(pet_number_stat[0].text.split("\n")[1].split(" ")[1])) == pet_number, 'Not all pets are displayed on '
+                                                                                      'the page')
+
+    # More then a half of pets has photo number
+    expect(images_counter >= pet_number / 2, 'Less then a half of pets has photo number')
+
+    # all pets has different names
+    expect(len(names) == len(names_set), 'Match of pet names')
+
+    # all pets has different set of name, breed and age
+    expect(pet_number == len(pets_list), 'Not all of pets has different set of name, breed and age')
+
+    assert_expectations()
+
+
+
 def test_petfriends(web_browser):
     web_browser.get("https://petfriends1.herokuapp.com/")
 
@@ -34,73 +106,3 @@ def test_petfriends(web_browser):
 
     # time.sleep(4)  # just for demo purposes, do NOT repeat it on real projects!
     assert web_browser.current_url == 'https://petfriends1.herokuapp.com/all_pets',"login error"
-
-
-@pytest.fixture(autouse=True)
-def testing():
-    chrome_options = Options()
-    chrome_options.headless = True
-    chrome_options.add_argument("--window-size=1280,800")
-    pytest.driver = webdriver.Chrome('./webdriver/chrome/chromedriver.exe', options=chrome_options)
-
-    # Переходим на страницу авторизации
-    pytest.driver.get('http://petfriends1.herokuapp.com/login')
-
-    yield
-
-    pytest.driver.quit()
-
-def test_show_my_pets():
-    # add email
-    pytest.driver.find_element_by_id('email').send_keys('test123@yandex.ru')
-    # add password
-    pytest.driver.find_element_by_id('pass').send_keys('123456')
-    # click submit button
-    pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
-    # Проверяем, что мы оказались на главной странице пользователя
-    assert pytest.driver.find_element_by_tag_name('h1').text == "PetFriends"
-    # Переходим на страницу /my_pets
-    pytest.driver.find_element_by_xpath('//li/a[@href="/my_pets"]').click()
-    # Проверка тайтла страницы /my_pets
-    assert pytest.driver.title == "PetFriends: My Pets"
-    title = pytest.driver.find_element_by_css_selector('head title')
-    assert title.get_attribute("innerText") == "PetFriends: My Pets"
-
-    # сбор значений проверяемых элементов
-    pet_number = len(pytest.driver.find_elements_by_css_selector("table.table-hover tbody > tr"))
-    images = pytest.driver.find_elements_by_css_selector("tbody tr th img")
-    names = pytest.driver.find_elements_by_xpath("//div[@id='all_my_pets']/table/tbody/tr/td[1]")
-    breeds = pytest.driver.find_elements_by_xpath("//div[@id='all_my_pets']/table/tbody/tr/td[2]")
-    ages = pytest.driver.find_elements_by_xpath("//div[@id='all_my_pets']/table/tbody/tr/td[3]")
-
-    images_counter = 0
-    names_set = set()
-    pets_list = set()
-    for i in range(pet_number):
-        
-        names_set.add(names[i].text)
-        pet = (names[i].text, breeds[i].text, ages[i].text)
-        pets_list.add(pet)
-        
-        if images[i].get_attribute('src') != '':
-            images_counter +=1
-        expect(images[i].get_attribute('src') != '', 'Pet has no picture')
-        expect(names[i].text != '', 'Empty name field')
-        expect(breeds[i].text != '', 'Empty name field')
-        expect(ages[i].text != '', 'Empty age field')
-
-    # all pets are displayed on the page
-    pet_number_stat = pytest.driver.find_elements_by_xpath("/html/body/div[1]//div[@class='.col-sm-4 left']")
-    expect((int(pet_number_stat[0].text.split("\n")[1].split(" ")[1])) == pet_number, 'Not all pets are displayed on '
-                                                                                      'the page')
-
-    # More then a half of pets has photo number
-    expect(images_counter >= pet_number / 2, 'Less then a half of pets has photo number')
-
-    # all pets has different names
-    expect(len(names) == len(names_set), 'Match of pet names')
-
-    # all pets has different set of name, breed and age
-    expect(pet_number == len(pets_list), 'Not all of pets has different set of name, breed and age')
-
-    assert_expectations()
